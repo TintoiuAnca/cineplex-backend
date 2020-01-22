6657788d680e2bdb060ad96b174276116b7b5c1d
Tintoiu Anca-Mihaela <anca.tintoiu.ext@siemens.com>
2020-01-22 17:21:53 +0200

    updated mysql connection properties

--- src/main/java/com/ctbav/internship/cineplexbackend/CineplexBackendApplication.java
+++ src/main/java/com/ctbav/internship/cineplexbackend/CineplexBackendApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CineplexBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CineplexBackendApplication.class, args);
+		SpringApplication.run(CineplexBackendApplication.class, args);	
 	}
 
 }
