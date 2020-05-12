ca76333f07a24fab45163a526a78fd27cba928ed
Tintoiu Anca-Mihaela <anca.tintoiu.ext@siemens.com>
2020-05-12 21:13:21 +0300

    checked auth0

--- src/main/java/com/ctbav/internship/cineplexbackend/config/SecurityConfiguration.java
+++ src/main/java/com/ctbav/internship/cineplexbackend/config/SecurityConfiguration.java
@@ -11,41 +11,41 @@ import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 @EnableWebSecurity
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-	@Value(value = "${auth0.apiAudience}")
-	private String apiAudience;
-	@Value(value = "${auth0.issuer}")
-	private String issuer;
+    @Value(value = "${auth0.apiAudience}")
+    private String apiAudience;
+    @Value(value = "${auth0.issuer}")
+    private String issuer;
 
-	protected void configure(HttpSecurity http) throws Exception {
-		JwtWebSecurityConfigurer.forRS256(apiAudience, issuer).configure(http).authorizeRequests()
-				.antMatchers(HttpMethod.GET, "/api/v1/movies").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/v1/movies/**").permitAll()
-//				.antMatchers(HttpMethod.GET, "/api/v1/history").permitAll()
-//				.antMatchers(HttpMethod.GET, "/api/v1/payment").permitAll()
-//				.antMatchers(HttpMethod.GET, "/api/v1/reservation").permitAll()
-//				.antMatchers(HttpMethod.GET, "/api/v1/user").permitAll()
-//				.antMatchers(HttpMethod.GET, "/api/v1/ticket").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/v1/room").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/v1/seat").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/v1/seat/**").permitAll()
-//				.antMatchers(HttpMethod.GET, "/api/v1/statistic").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/v1/schedule").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/v1/schedule/**").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/v1/usertype").permitAll()
-//				.antMatchers(HttpMethod.GET, "/api/v1/movies/**").permitAll()
-				.antMatchers(HttpMethod.POST, "/api/v1/movies").permitAll()
-//				.antMatchers(HttpMethod.POST, "/api/v1/history").permitAll()
-//				.antMatchers(HttpMethod.POST, "/api/v1/payment").permitAll()
-//				.antMatchers(HttpMethod.POST, "/api/v1/reservation").permitAll()
-				.antMatchers(HttpMethod.POST, "/api/v1/user").permitAll()
-//				.antMatchers(HttpMethod.POST, "/api/v1/ticket").permitAll()
-				.antMatchers(HttpMethod.POST, "/api/v1/room").permitAll()
-				.antMatchers(HttpMethod.POST, "/api/v1/seat").permitAll()
-//				.antMatchers(HttpMethod.POST, "/api/v1/statistic").permitAll()
-				.antMatchers(HttpMethod.POST, "/api/v1/schedule").permitAll()
-				.antMatchers(HttpMethod.POST, "/api/v1/usertype").permitAll()
-//				.antMatchers(HttpMethod.POST, "/api/v1/movies/**").permitAll()
-				.antMatchers(HttpMethod.GET, "/api/v1/user").hasAuthority("view:users").anyRequest().authenticated();
-	}
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtWebSecurityConfigurer.forRS256(apiAudience, issuer).configure(http).authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/v1/movies").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/movies/**").permitAll()
+//              .antMatchers(HttpMethod.GET, "/api/v1/history").permitAll()
+//              .antMatchers(HttpMethod.GET, "/api/v1/payment").permitAll()
+//              .antMatchers(HttpMethod.GET, "/api/v1/reservation").permitAll()
+//              .antMatchers(HttpMethod.GET, "/api/v1/user").permitAll()
+//              .antMatchers(HttpMethod.GET, "/api/v1/ticket").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/room").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/seat").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/seat/**").permitAll()
+//              .antMatchers(HttpMethod.GET, "/api/v1/statistic").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/schedule").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/schedule/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/usertype").permitAll()
+//              .antMatchers(HttpMethod.GET, "/api/v1/movies/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/movies").permitAll()
+//              .antMatchers(HttpMethod.POST, "/api/v1/history").permitAll()
+//              .antMatchers(HttpMethod.POST, "/api/v1/payment").permitAll()
+//              .antMatchers(HttpMethod.POST, "/api/v1/reservation").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/user").permitAll()
+//              .antMatchers(HttpMethod.POST, "/api/v1/ticket").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/room").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/seat").permitAll()
+//              .antMatchers(HttpMethod.POST, "/api/v1/statistic").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/schedule").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/usertype").permitAll()
+//              .antMatchers(HttpMethod.POST, "/api/v1/movies/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/api/v1/user").hasAuthority("view:users").anyRequest().authenticated();
+    }
 
 }
