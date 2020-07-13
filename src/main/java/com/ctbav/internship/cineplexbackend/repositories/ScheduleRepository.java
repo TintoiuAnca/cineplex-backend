801268f90873fbd2ab2a86176c8ae1a954b8b82d
Tintoiu Anca-Mihaela <anca.tintoiu.ext@siemens.com>
2020-07-13 18:31:52 +0300

    created method to filter schedules by current date

--- src/main/java/com/ctbav/internship/cineplexbackend/repositories/ScheduleRepository.java
+++ src/main/java/com/ctbav/internship/cineplexbackend/repositories/ScheduleRepository.java
@@ -3,12 +3,13 @@ package com.ctbav.internship.cineplexbackend.repositories;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ctbav.internship.cineplexbackend.models.Movie;
 import com.ctbav.internship.cineplexbackend.models.Schedule;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-	List<Schedule> findAllByScheduledMovie(Movie movie);
+	List<Schedule> findAllByScheduledMovie(Optional<Movie> movie);
 	List<Schedule> findAllByDate(Date date);
 
 }
