571816e3d91d50674d2897d6b92c986ff89e2b0e
Tintoiu Anca-Mihaela <anca.tintoiu.ext@siemens.com>
2020-07-13 18:32:11 +0300

    commit

--- src/main/java/com/ctbav/internship/cineplexbackend/controllers/ScheduleController.java
+++ src/main/java/com/ctbav/internship/cineplexbackend/controllers/ScheduleController.java
@@ -3,10 +3,15 @@ package com.ctbav.internship.cineplexbackend.controllers;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
+import java.util.TimeZone;
 import java.util.stream.Collectors;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -27,63 +32,85 @@ import com.ctbav.internship.cineplexbackend.util.ComparatorSchedule;
 @RestController
 @RequestMapping("/api/v1/schedule")
 public class ScheduleController {
-	@Autowired
-	private ScheduleRepository scheduleRepository;
+  private static final Date CurrentDate = new Date();
+  private static final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd",Locale.ENGLISH);
+  private static final String stringCurrentDate=formatter.format(CurrentDate);
+  private static final ComparatorSchedule comparatorSchedule = new ComparatorSchedule();
+  
+  @Autowired
+  private ScheduleRepository scheduleRepository;
 
-	@Autowired
-	private MovieRepository movieRepository;
+  @Autowired
+  private MovieRepository movieRepository;
 
-	@Autowired
-	public ScheduleController(ScheduleRepository scheduleRepo, MovieRepository movieRepo) {
-		this.scheduleRepository = scheduleRepo;
-		this.movieRepository = movieRepo;
-	}
+  @Autowired
+  public ScheduleController(ScheduleRepository scheduleRepo, MovieRepository movieRepo) {
+    this.scheduleRepository = scheduleRepo;
+    this.movieRepository = movieRepo;
+  }
 
-	@GetMapping
-	public List<ScheduleDTO> list() throws ParseException {
-		ComparatorSchedule comparatorSchedule = new ComparatorSchedule();
-		return scheduleRepository.findAll().stream().sorted(comparatorSchedule::compare).map(t -> {
-			try {
-				return new ScheduleDTO(t);
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			return null;
-		}).collect(Collectors.toList());
-	}
+//  @GetMapping
+//  public List<ScheduleDTO> list() throws ParseException {
+//    ComparatorSchedule comparatorSchedule = new ComparatorSchedule();
+//    return scheduleRepository.findAll().stream().sorted(comparatorSchedule::compare).map(t -> {
+//      try {
+//        return new ScheduleDTO(t);
+//      } catch (ParseException e) {
+//        // TODO Auto-generated catch block
+//        e.printStackTrace();
+//      }
+//      return null;
+//    }).collect(Collectors.toList());
+//  }
 
-	@PostMapping
-	@ResponseStatus(HttpStatus.OK)
-	public Schedule create(@RequestBody ScheduleDTO scheduleDto) throws ParseException {
-		Schedule schedule = new Schedule(scheduleDto);
-		scheduleRepository.save(schedule);
-		return schedule;
-	}
+  @PostMapping
+  @ResponseStatus(HttpStatus.OK)
+  public Schedule create(@RequestBody ScheduleDTO scheduleDto) throws ParseException {
+    Schedule schedule = new Schedule(scheduleDto);
+    scheduleRepository.save(schedule);
+    return schedule;
+  }
 
-	@GetMapping("/{id}")
-	public List<ScheduleDTO> getScheduleByMovie(@PathVariable("id") long id) throws ParseException {
-		Optional<Movie> movie = movieRepository.findById(id);// find the movie using the id in the URL
-		ComparatorSchedule comparatorSchedule = new ComparatorSchedule();
-		return scheduleRepository.findAllByScheduledMovie(movie.get()).stream().sorted(comparatorSchedule::compare)
-				.map(t -> {
-					try {
-						return new ScheduleDTO(t);
-					} catch (ParseException e) {
-						e.printStackTrace();
-					}
-					return null;
-				}).collect(Collectors.toList());
-//		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd"); //get the current date
-//		Date date = new Date();
-//		String strDate = formatter.format(date);
-//		Date data = formatter.parse(strDate);
-//		System.out.println("THIS IS THE DATE:" + data);
-	}
+  @GetMapping("/{id}")
+  @Temporal(TemporalType.DATE)
+  public List<ScheduleDTO> getScheduleByMovieAndCurrentDate(@PathVariable("id") long id)
+      throws ParseException {
+    Optional<Movie> movie = movieRepository.findById(id);
+    List<ScheduleDTO> schedulesDTO = new ArrayList<ScheduleDTO>();
+    List<Schedule> schedules = scheduleRepository.findAllByScheduledMovie(movie);
+    for (Schedule s : schedules) {
+      String stringDate=formatter.format(s.getDate());
+      if (stringCurrentDate.equals(stringDate)) {
+        ScheduleDTO scheduleDTO = new ScheduleDTO(s);
+        schedulesDTO.add(scheduleDTO);
+      }
+    }
+    return schedulesDTO.stream().sorted(comparatorSchedule::compare).collect(Collectors.toList());
+  }
+  
+  @GetMapping("/{id}")
+  @RequestMapping("changeScheduleByDate")
+  @Temporal(TemporalType.DATE)
+  public List<ScheduleDTO> getScheduleByMovieAndDate(@PathVariable("id") long id,@RequestBody Date date)
+      throws ParseException {
+    System.err.println("I'm here");
+    Optional<Movie> movie = movieRepository.findById(id);
+    List<ScheduleDTO> schedulesDTO = new ArrayList<ScheduleDTO>();
+    List<Schedule> schedules = scheduleRepository.findAllByScheduledMovie(movie);
+    for (Schedule s : schedules) {
+      String stringDate=formatter.format(date);
+      String databaseDateString=formatter.format(s.getDate());
+      if (databaseDateString.equals(stringDate)) {
+        ScheduleDTO scheduleDTO = new ScheduleDTO(s);
+        schedulesDTO.add(scheduleDTO);
+      }
+    }
+    return schedulesDTO.stream().sorted(comparatorSchedule::compare).collect(Collectors.toList());
+  }
 
-	@DeleteMapping("/{id}")
-	public void delete(@PathVariable String id) {
-		this.scheduleRepository.deleteById(Long.parseLong(id));
-	}
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable String id) {
+    this.scheduleRepository.deleteById(Long.parseLong(id));
+  }
 
 }
