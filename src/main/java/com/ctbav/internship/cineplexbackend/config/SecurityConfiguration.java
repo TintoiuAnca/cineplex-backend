b9977df335b2c79ada348bf5c16fee4f77abf7ac
Tintoiu Anca-Mihaela <anca.tintoiu.ext@siemens.com>
2020-07-13 18:30:50 +0300

    minor enhacements

--- src/main/java/com/ctbav/internship/cineplexbackend/config/SecurityConfiguration.java
+++ src/main/java/com/ctbav/internship/cineplexbackend/config/SecurityConfiguration.java
@@ -1,11 +1,16 @@
 package com.ctbav.internship.cineplexbackend.config;
 
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
 
 @EnableWebSecurity
@@ -15,6 +20,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     private String apiAudience;
     @Value(value = "${auth0.issuer}")
     private String issuer;
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("Authorization");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     protected void configure(HttpSecurity http) throws Exception {
         JwtWebSecurityConfigurer.forRS256(apiAudience, issuer).configure(http).authorizeRequests()
