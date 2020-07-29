2c02b45bbc79cc40440adf7bf3687302ade2bdc1
Tintoiu Anca-Mihaela <anca.tintoiu.ext@siemens.com>
2020-07-29 15:19:52 +0300

    added method

--- src/main/java/com/ctbav/internship/cineplexbackend/repositories/RoomRepository.java
+++ src/main/java/com/ctbav/internship/cineplexbackend/repositories/RoomRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.ctbav.internship.cineplexbackend.models.Room;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+  Room findByRoomName(String room);
 
 }
