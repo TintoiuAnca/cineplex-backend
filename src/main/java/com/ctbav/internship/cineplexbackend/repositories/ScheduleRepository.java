28c008275250283980da7585b96edf8aef54668b
Tintoiu Anca-Mihaela <anca.tintoiu.ext@siemens.com>
2020-07-29 15:20:49 +0300

    update

--- src/main/java/com/ctbav/internship/cineplexbackend/repositories/ScheduleRepository.java
+++ src/main/java/com/ctbav/internship/cineplexbackend/repositories/ScheduleRepository.java
@@ -6,10 +6,10 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ctbav.internship.cineplexbackend.models.Movie;
+import com.ctbav.internship.cineplexbackend.models.Room;
 import com.ctbav.internship.cineplexbackend.models.Schedule;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 	List<Schedule> findAllByScheduledMovie(Optional<Movie> movie);
 	List<Schedule> findAllByDate(Date date);
-
 }
