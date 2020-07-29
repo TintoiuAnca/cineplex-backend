caf61f047433829955d7de6d5cecaafe32093d3b
Tintoiu Anca-Mihaela <anca.tintoiu.ext@siemens.com>
2020-07-29 15:18:55 +0300

    updated method

--- src/main/java/com/ctbav/internship/cineplexbackend/repositories/SeatRepository.java
+++ src/main/java/com/ctbav/internship/cineplexbackend/repositories/SeatRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ctbav.internship.cineplexbackend.models.Movie;
+import com.ctbav.internship.cineplexbackend.models.Room;
 import com.ctbav.internship.cineplexbackend.models.Seat;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
-	List<Seat> findAllByMovies(Optional<Movie> movie);
-
+	List<Seat> findAllByMoviesAndRooms(Optional<Movie> movie,Room room);
 }
